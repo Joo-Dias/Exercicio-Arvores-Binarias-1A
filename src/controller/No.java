5b6770b4408bfb151beb56d3e6eead58cb25cfc0
João Dias <joaovitorelf@gmail.com>
2023-05-29 13:18:04 -0300

    Finalizando o exercício

---
+++ src/controller/No.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class No {
+
+	public int dado;
+	public No direita;
+	public No esquerda;
+
+	@Override
+	public String toString() {
+		return "No [dado=" + dado + "]";
+	}
+}
